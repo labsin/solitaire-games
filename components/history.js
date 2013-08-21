32840b32b16977ff48b8f09dd4b271ded05fa6ae
Sam Segers <sam.sgrs@gmail.com>
2013-08-21 13:57:19 +0200

    history: Can be initiated with a previous history

--- components/history.js
+++ components/history.js
@@ -1,8 +1,19 @@
 var history
 
-var History = function() {
+var History = function(savedGame) {
+    if(!savedGame) {
+        savedGame = []
+    }
+    print("History::"+savedGame.toString())
     this.marking = false
-    this.json = []
+    this.json = savedGame
+    if(!this.json.length) {
+        historyLength = 0
+    }
+    else {
+        historyLength = this.json.length
+    }
+    historyIndex = 0
 }
 
 History.prototype.addToHistory = function (fromIndex, fromStack, fromUp, toIndex, toStack, toUp, flipZ) {
@@ -14,7 +25,7 @@ History.prototype.addToHistory = function (fromIndex, fromStack, fromUp, toIndex
 }
 
 History.prototype.startMove = function () {
-    print("History::startMove")
+    print("History::startMove "+historyIndex)
     this.marking = true
     var iii = 0
     while(this.json[historyIndex+iii]) {
@@ -25,7 +36,7 @@ History.prototype.startMove = function () {
 }
 
 History.prototype.endMove = function () {
-    print("History::endMove")
+    print("History::endMove "+historyIndex)
     if(this.json[historyIndex] && this.json[historyIndex].length>0) {
         historyIndex++
         historyLength = this.json.length
@@ -53,6 +64,8 @@ History.prototype.returnAndGoForward = function () {
     return tmp
 }
 
-function init() {
-    history = new History()
+function init(savedGame) {
+    if(typeof savedGame == 'undefined')
+        savedGame = []
+    history = new History(savedGame)
 }
