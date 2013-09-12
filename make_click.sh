150c2c17606fb97e168d6acddb89fcc7bf43288e
Sam Segers <sam.sgrs@gmail.com>
2013-09-12 23:33:25 +0200

    Remove more unneded folders and files when making a click package

--- make_click.sh
+++ make_click.sh
@@ -5,11 +5,13 @@ rm -rf $BUILDDIR
 cp -r $FROMDIR $BUILDDIR
 echo "Entering ${BUILDDIR}"
 cd $BUILDDIR
-rm -rf .git* debian desktop solitaire-games.qmlp* solitaire-games128.png solitaire-games16.png solitaire-games32.png solitaire-games64.png make_click.sh
+rm -rf .git* debian desktop solitaire-games.qmlp* solitaire-games128.png solitaire-games16.png solitaire-games32.png solitaire-games64.png make_click.sh README.md make_click.sh LICENSE
 cd po
 echo "Generating mo files"
 ./generate_mo.sh
-cd ../..
+cd ..
+rm -r po
+cd ..
 echo "Build click package in `pwd`"
 click build $BUILDDIR
 cd $FROMDIR
